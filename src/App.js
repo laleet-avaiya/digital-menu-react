32b3df013acefdc59820b0230f7923eaa1605887
lmavaiya <42064785+lmavaiya@users.noreply.github.com>
2020-04-10 22:35:35 +0530

    Icon Added

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import firebase from './components/firebase';
-import Card from './components/Card';
-import ItemList from './components/ItemList';
+import React, { Component } from "react";
+import "./App.css";
+import firebase from "./components/firebase";
+import Card from "./components/Card";
+import ItemList from "./components/ItemList";
 
-const restaurantData = {
-  name: "Ashish Restaurant",
-  address: "102 - 103, Sai Residency, Amroli Katargam Road, Near Gajera School, Katargam, Surat",
-  menu: [
-    {
-      type: "starter",
-      items: [
-        {
-          itemName: "manchurian",
-          price: 50
-        }
-      ]
-    },
-    {
-      type: "main course",
-      items: [
-        {
-          itemName: "panir butter masala",
-          price: 40
-        }
-      ]
-    },
-    {
-      type: "drink",
-      items: [
-        {
-          itemName: "tea",
-          price: 20
-        }
-      ]
-    },
-  ]
-}
 class App extends Component {
   constructor() {
     super();
     this.state = {
       selectCard: -1,
       // selectCard:0,
-    }
+    };
 
     this.changeCard = this.changeCard.bind(this);
     this.sentenceCase = this.sentenceCase.bind(this);
@@ -56,47 +23,43 @@ class App extends Component {
 
   handleChange(e) {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   }
 
   titleCase(str) {
-    str = str.toLowerCase().split(' ');
+    str = str.toLowerCase().split(" ");
     for (var i = 0; i < str.length; i++) {
       str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
     }
-    return str.join(' ');
+    return str.join(" ");
   }
 
   sentenceCase(str) {
-    return str.replace(/[a-z]/i, function (letter) {
-
-      return letter.toUpperCase();
-
-    }).trim();
+    return str
+      .replace(/[a-z]/i, function (letter) {
+        return letter.toUpperCase();
+      })
+      .trim();
   }
 
   componentDidMount() {
-    
     var url = new URL(window.location.href);
     var restaurant_id = url.searchParams.get("restaurant_id");
-    if(restaurant_id == null)
-    {
-        restaurant_id = prompt("Restaurant Id?");
+    if (restaurant_id == null) {
+      restaurant_id = prompt("Restaurant Id?");
     }
-    if(restaurant_id){
-      const itemsRef = firebase.database().ref('restaurant/' + restaurant_id);
-      itemsRef.on('value', (snapshot) => {
+    if (restaurant_id) {
+      const itemsRef = firebase.database().ref("restaurant/" + restaurant_id);
+      itemsRef.on("value", (snapshot) => {
         let items = snapshot.val();
         console.log(items);
         this.setState({
-          data: items
+          data: items,
         });
       });
     }
   }
-
-
 
   render() {
     var { data, selectCard } = this.state;
@@ -106,19 +69,26 @@ class App extends Component {
         <div className="home">
           <nav className="navbar navbar-light bg-success text-white justify-content-between">
             <header>
-              <h5 className="restaurant-Name">{this.titleCase(restaurantData.name.toLowerCase())}</h5>
-              <p className="restaurant-address">{this.titleCase(restaurantData.address.toLowerCase())}</p>
+              <h5 className="restaurant-Name">
+                {this.titleCase(data.restaurant_name.toLowerCase())}
+              </h5>
+              <p className="restaurant-address">
+                {this.titleCase(data.restaurant_address.toLowerCase())}
+              </p>
             </header>
           </nav>
           <div className="restaurant-detail">
             <div className="menu-list">
-              {
-
-                data.menu.map((menu, index) => {
-                  return <Card key={index} index={index} menu={menu} changeCard={this.changeCard}></Card>
-                }
-                )
-              }
+              {data.menu.map((menu, index) => {
+                return (
+                  <Card
+                    key={index}
+                    index={index}
+                    menu={menu}
+                    changeCard={this.changeCard}
+                  ></Card>
+                );
+              })}
             </div>
           </div>
         </div>
@@ -126,17 +96,24 @@ class App extends Component {
     } else if (data && selectCard >= 0) {
       return (
         <div>
-          <nav className="navbar navbar-light bg-success text-white justify-content-between">
-            <h6 className="restaurant-Name">{this.titleCase(data.menu[selectCard].type.toLowerCase())}</h6>
-            <button className="btn btn-outline-warning pull-right " type="button" onClick={() => this.changeCard(-1)}><b>Home</b></button>
+          <nav className="navbar navbar-light bg-success text-white justify-content-between sticky-top ">
+            <h6 className="restaurant-Name">
+              {this.titleCase(data.menu[selectCard].type.toLowerCase())}
+            </h6>
+            <button
+              className="btn btn-outline-warning pull-right "
+              type="button"
+              onClick={() => this.changeCard(-1)}
+            >
+              <b>Home</b>
+            </button>
           </nav>
           <div className="restaurant-detail">
-            <ItemList menu={data.menu[selectCard]} ></ItemList>
+            <ItemList menu={data.menu[selectCard]}></ItemList>
           </div>
         </div>
       );
-    }
-    else {
+    } else {
       return (
         <div>
           <h2>Loading ... </h2>
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,62 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class ItemList extends Component {
-    constructor() {
-        super();
-        this.state = {
-        }
+  constructor() {
+    super();
+    this.state = {};
 
-        this.sentenceCase = this.sentenceCase.bind(this);
-        this.titleCase = this.titleCase.bind(this);
+    this.sentenceCase = this.sentenceCase.bind(this);
+    this.titleCase = this.titleCase.bind(this);
+  }
 
+  titleCase(str) {
+    str = str.toLowerCase().split(" ");
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
     }
+    return str.join(" ");
+  }
 
-    titleCase(str) {
-        str = str.toLowerCase().split(' ');
-        for (var i = 0; i < str.length; i++) {
-            str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
-        }
-        return str.join(' ');
-    }
+  sentenceCase(str) {
+    return str
+      .replace(/[a-z]/i, function (letter) {
+        return letter.toUpperCase();
+      })
+      .trim();
+  }
 
-    sentenceCase(str) {
-        return str.replace(/[a-z]/i, function (letter) {
+  render() {
+    const menu = this.props.menu;
+    return (
+      <div>
+        {menu.items.map((item, index) => (
+          <div key={index} className="item-card">
+            <div className="item-container">
+              <p className="pull-left">
+                <b>{this.titleCase(item.itemName.toLowerCase()) + "  "}</b>
+              </p>
+              {item.veg ? (
+                <p className="pull-right">
+                  <span className="veg-indian-vegetarian"></span>
+                </p>
+              ) : (
+                <p className="pull-right">
+                  <span className="non-veg-indian-vegetarian"></span>
+                </p>
+              )}
+              <br />
 
-            return letter.toUpperCase();
-
-        }).trim();
-    }
-
-    render() {
-        const menu = this.props.menu;
-        return (
-            <div>
-                {
-                    menu.items.map((item, index) =>
-                        <div key={index} className="item-card">
-                            <div className="item-container">
-
-                                <p className="pull-left"><b>{this.titleCase(item.itemName.toLowerCase()) + "  "}</b></p>
-                                {index % 2 === 1 ? <p className="pull-right"><span className="veg-indian-vegetarian"></span></p> : <span></span>}
-
-                                {index % 2 === 0 ? <p className="pull-right"><span className="veg-indian-vegetarian"></span></p> : <span></span>}
-                                <br />
-
-                                <p className="item-price">{"Rs. " + item.price}</p>
-                                {/* <p><b>{item.itemName + "  "}</b></p>
+              <p className="item-price">{"Rs. " + item.price}</p>
+              {/* <p><b>{item.itemName + "  "}</b></p>
                                 {index % 2 === 1 ? <p className="badge badge-success">Veg</p> : <span></span>}
                                 {index % 2 === 0 ? <p className="badge badge-danger">Non Veg</p> : <span></span>}
                                 {index % 3 === 0 ? <p className="badge badge-info">Pure Jain</p> : <span></span>}
                                 <p><b>{item.itemName + "  "}</b><span className="veg-indian-vegetarian"></span></p> */}
-
-
-                            </div>
-                        </div>
-                    )
-                }
             </div>
-
-
-        )
-    }
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
